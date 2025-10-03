2d625ee19fcbaa0d7e137162c678e6cfbd0f2cc7
Yuriy Maslenskiy <maslenskiy2000@mail.ru>
2025-10-03 18:52:04 +0300

    Реализовал открытие и закрытин модального окна по клику на юзера

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,27 +1,34 @@
+import { useState } from 'react'
+
 const Header = () => {
-    return (
-      <header className="header">
-        <div className="container">
-          <div className="header__block">
-            <div className="header__logo _show _light">
-              <a href="" target="_self">
-                <img src="images/logo.png" alt="logo" />
-              </a>
-            </div>
-            <div className="header__logo _dark">
-              <a href="" target="_self">
-                <img src="images/logo_dark.png" alt="logo" />
-              </a>
-            </div>
-            <nav className="header__nav">
-              <button className="header__btn-main-new _hover01" id="btnMainNew">
-                <a href="#popNewCard">Создать новую задачу</a>
-              </button>
-              <a href="#user-set-target" className="header__user _hover02">
-                Ivan Ivanov
-              </a>
+  const [isOpen, setIsOpen] = useState(false)
+
+  const openModal = () => {
+    setIsOpen((prevState) => !prevState)
+  }
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="header__block">
+          <div className="header__logo _show _light">
+            <a href="" target="_self">
+              <img src="images/logo.png" alt="logo" />
+            </a>
+          </div>
+          <div className="header__logo _dark">
+            <a href="" target="_self">
+              <img src="images/logo_dark.png" alt="logo" />
+            </a>
+          </div>
+          <nav className="header__nav">
+            <button className="header__btn-main-new _hover01" id="btnMainNew">
+              <a href="#popNewCard">Создать новую задачу</a>
+            </button>
+            <a onClick={openModal} href="#user-set-target" className="header__user _hover02">
+              Ivan Ivanov
+            </a>
+            {isOpen && (
               <div className="header__pop-user-set pop-user-set" id="user-set-target">
-                <a href="">x</a>
                 <p className="pop-user-set__name">Ivan Ivanov</p>
                 <p className="pop-user-set__mail">ivan.ivanov@gmail.com</p>
                 <div className="pop-user-set__theme">
@@ -32,10 +39,11 @@ const Header = () => {
                   <a href="#popExit">Выйти</a>
                 </button>
               </div>
-            </nav>
-          </div>
+            )}
+          </nav>
         </div>
-      </header>
-    )
+      </div>
+    </header>
+  )
 }
-export default Header;
+export default Header
